d7a421a54262d8b52eae4815e28755016fb604cc
Marcos Rosales <marcostadeorosales@gmail.com>
2022-09-24 00:05:43 -0300

    Finalizado Trabajar Producto

--- ClasesBase/TrabajarProducto.cs
+++ ClasesBase/TrabajarProducto.cs
@@ -9,6 +9,10 @@ namespace ClasesBase
 {
     public class TrabajarProducto
     {
+        private static SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.muebleriaConection);
+        private static SqlCommand cmd = new SqlCommand();
+        private static SqlDataAdapter da = new SqlDataAdapter();
+
         public DataTable TraerProductos()
         {
             SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.muebleriaConection);
@@ -24,5 +28,53 @@ namespace ClasesBase
 
             return dt;
         }
+
+        public static void InsertarProducto(Producto producto)
+        {
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_prod_insertar";
+            cmd.Connection = cnn;
+
+            //cmd.Parameters.AddWithValue("@codigo",producto.CodProducto);  Depende de si funciona lo de Clave Primaria
+            cmd.Parameters.AddWithValue("@categoria", producto.Categoria);
+            cmd.Parameters.AddWithValue("@color", producto.Color);
+            cmd.Parameters.AddWithValue("@descripcion", producto.Descripcion);
+            cmd.Parameters.AddWithValue("@precio", producto.Precio);
+
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        public static void EliminarProducto(Producto producto)
+        {
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_prod_eliminar";
+            cmd.Connection = cnn;
+
+            cmd.Parameters.AddWithValue("@codigo", producto.CodProducto);
+
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
+        public static void ModificarProducto(Producto producto)
+        {
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "sp_prod_modificar";
+            cmd.Connection = cnn;
+
+            cmd.Parameters.AddWithValue("@codigo", producto.CodProducto);
+            cmd.Parameters.AddWithValue("@categoria", producto.Categoria);
+            cmd.Parameters.AddWithValue("@color", producto.Color);
+            cmd.Parameters.AddWithValue("@descripcion", producto.Descripcion);
+            cmd.Parameters.AddWithValue("@precio", producto.Precio);
+
+            cnn.Open();
+            cmd.ExecuteNonQuery();
+            cnn.Close();
+        }
+
     }
 }
