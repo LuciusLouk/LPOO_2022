0e2e106450eb262b9420b27baf20c88d9fecba2c
FranLeo98 <franlml2012@gmail.com>
2022-09-24 01:53:27 -0300

    Avance(?) del punto 3 del tp3

---
+++ ClasesBase/ConversorDeEstados.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace ClasesBase
+{
+    public class ConversorDeEstados : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value.ToString().ToLower())
+            {
+                case "pendiente":
+                    return "Red";
+                case "pagada":
+                    return "Green";
+                case "contabilizada":
+                    return "Blue";
+                case "anulada":
+                    return "Gray";
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+                CultureInfo culture)
+        {
+            
+            throw new NotImplementedException();
+        }
+    }
+}
