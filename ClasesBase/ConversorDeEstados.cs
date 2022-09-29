f25aa091865b0b78ff623465021bffa188187608
Lucas Mesconi <mesconilucas2877@gmail.com>
2022-09-29 18:07:51 -0300

    Corregido Punto 3 Mesconi

--- ClasesBase/ConversorDeEstados.cs
+++ ClasesBase/ConversorDeEstados.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
-using System.Windows;
 using System.Windows.Data;
-using System.Globalization;
+using System.Windows;
+using System.Windows.Media.Animation;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace ClasesBase
 {
@@ -12,18 +20,21 @@ namespace ClasesBase
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            switch (value.ToString().ToLower())
+            if (value != null) 
             {
-                case "pendiente":
-                    return "Red";
-                case "pagada":
-                    return "Green";
-                case "contabilizada":
-                    return "Blue";
-                case "anulada":
-                    return "Gray";
+                switch (value.ToString())
+                {
+                    case "PENDIENTE":
+                        return new SolidColorBrush(Colors.Red);
+                    case "PAGADA":
+                        return new SolidColorBrush(Colors.Green);
+                    case "CONTABILIZADA":
+                        return new SolidColorBrush(Colors.Blue);
+                    case "ANULADA":
+                        return new SolidColorBrush(Colors.Gray);
+                }
             }
-            return value;
+            return new SolidColorBrush(Colors.White);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter,
